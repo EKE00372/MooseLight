2c5c98c284be668e4d6e53a45dc7c3798f9e56f0
EK <eke00372@gmail.com>
2018-10-02 08:14:13 +0800

    Update MooseLight.lua

--- MooseLight.lua
+++ MooseLight.lua
@@ -1,34 +1,43 @@
--- This file is loaded from "MooseLight.toc"
-
 -- [[ 根據地點調整亮度 ]] --
 
-local ID = {
-	--[85] = true,	-- 奧格瑪
-	[86] = true,	-- 奧格瑪暗影裂谷
-	
-	[1015] = true,	-- 威奎斯特莊園 / Waycrest Manor - The Grand Foyer 
-	[1016] = true,	-- 威奎斯特莊園 / Waycrest Manor - Upstairs
-	[1017] = true,	-- 威奎斯特莊園 / Waycrest Manor - The Cellar
-	[1018] = true,	-- 威奎斯特莊園 / Waycrest Manor - Catacombs
-	[1029] = true,	-- 威奎斯特莊園 / Waycrest Manor 底層
-	
-	[974] = true,	-- 托達戈爾 / Tol Dagor
-	[975] = true,	-- 托達戈爾 / Tol Dagor - The Drain
-	[976] = true,	-- 托達戈爾 / Tol Dagor - The Brig
-	[977] = true,	-- 托達戈爾 / Tol Dagor - Detention Block
-	[978] = true,	-- 托達戈爾 / Tol Dagor - Officer Quarters
-	[979] = true,	-- 托達戈爾 / Tol Dagor - Overseer's Redoubt
-	[980] = true,	-- 托達戈爾 / Tol Dagor - Overseer's Summit
-	
-	[1004] = true,	-- 諸王之眠
+-- add new map id by yourself:
+-- https://wow.gamepedia.com/UiMapID
 
-	[1169] = true,	-- 托達戈爾野外 / Tol Dagor
-	[1162] = true,	-- 波拉勒斯圍城戰
+local ID = {
+	-- Orgrimmar / 奧格瑪
+	[86] = true,	-- 暗影裂谷
+	
+	-- Drustvar / 佐司瓦
+	[896] = true,
+	
+	-- 威奎斯特莊園 / Waycrest Manor
+	[1015] = true,	-- The Grand Foyer 
+	[1016] = true,	-- Upstairs
+	[1017] = true,	-- The Cellar
+	[1018] = true,	-- Catacombs
+	[1029] = true,	-- 底層
+	
+	-- 托達戈爾 / Tol Dagor
+	[974] = true,
+	[975] = true,	-- The Drain
+	[976] = true,	-- The Brig
+	[977] = true,	-- Detention Block
+	[978] = true,	-- Officer Quarters
+	[979] = true,	-- Overseer's Redoubt
+	[980] = true,	-- Overseer's Summit
+	[1169] = true,
+	
+	-- Kings' Rest / 諸王之眠
+	[1004] = true,
+	
+	-- Siege of Boralus / 波拉勒斯圍城戰
+	[1162] = true,
 }
 
 local function changeGamma()
 	local MapId = C_Map.GetBestMapForUnit("player")
 	if MapId and ID[MapId] then
+		-- set gamma here
 		SetCVar("Gamma", 1.2)
 	else
 		SetCVar("Gamma", 1)
