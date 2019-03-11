2ecd25b9f0d7de7be89ac6092f1291ebfde6b43e
EK <eke00372@gmail.com>
2019-03-12 07:32:58 +0800

    clean up and......SOTS test?

--- MooseLight.lua
+++ MooseLight.lua
@@ -4,91 +4,87 @@
 -- https://wow.gamepedia.com/UiMapID
 -- macro to get your map id:
 -- /dump C_Map.GetBestMapForUnit("player")
+-- CVar Gamma/Brightness/Contrast
 
 local timeID = {
 	[896] = true,	-- Drustvar / 佐司瓦
 	[895] = true,	-- Tiragarde Sound / 提加拉德
 	[942] = true,	-- Stormsong Valley / 斯托頌恩
+
 	--[1196] = true,
 	--[1197] = true,
 	--[1198] = true,
-	
 }
 
-local ID = {
-	-- Orgrimmar / 奧格瑪
-	[86] = true,	-- 暗影裂谷
+local zoneID = {
+	[86] = true,	-- Orgrimmar / 奧格瑪暗影裂谷
+	--[1040] = true,	-- Shrine of the Storm / 風暴聖壇
+	[1004] = true,	-- Kings' Rest / 諸王之眠
+	[1162] = true,	-- Siege of Boralus / 波拉勒斯圍城戰
+	[1038] = true,	-- Temple of Sethraliss / 瑟沙利斯神廟
+	--[1042] = true,	-- Atrium of Sethraliss / 瑟沙利斯中庭
+	[1152] = true,		-- Uldir, Plague Vault / 奧迪爾維克提斯
 	
 	-- 威奎斯特莊園 / Waycrest Manor
-	[1015] = true,	-- The Grand Foyer 
+	[1015] = true,	-- The Grand Foyer
 	[1016] = true,	-- Upstairs
 	[1017] = true,	-- The Cellar
 	[1018] = true,	-- Catacombs
 	[1029] = true,	-- 底層
 	
 	-- 托達戈爾 / Tol Dagor
-	[974] = true,
+	[974] = true,	-- Tol Dagor
 	[975] = true,	-- The Drain
 	[976] = true,	-- The Brig
 	[977] = true,	-- Detention Block
 	[978] = true,	-- Officer Quarters
 	[979] = true,	-- Overseer's Redoubt
 	[980] = true,	-- Overseer's Summit
-	[1169] = true,
-	
-	-- Kings' Rest / 諸王之眠
-	[1004] = true,
-	
-	-- Siege of Boralus / 波拉勒斯圍城戰
-	[1162] = true,
-	
-	-- Temple of Sethraliss / 瑟沙利斯神廟
-	[1038] = true,
-	--[1042] = true,	-- Atrium of Sethraliss
-	
-	-- Shrine of the Storm / 風暴聖壇
-	--[1040] = true,
-	
-	-- Uldir / 奧迪爾
-	[1152] = true,	-- Plague Vault(維克提斯)
-	--[[
-	[1148] = true,	-- Ruin's Descent(一王)
-	[1149] = true,	-- Hall of Sanitation(二王)
-	[1150] = true,	-- Ring of Containment
-	[1151] = true,	-- Archives of Eternity(蟲)
-	[1153] = true,	-- Gallery of Failures(狗)
-	[1154] = true,	-- The Oblivion Door(祖爾)
-	[1155] = true,	-- The Festering Core(尾王)
-	]]--
+	[1169] = true,	-- Tol Dagor outside / 托達戈爾戶外	
+}
+
+local nameList = {
+	["潮水聖所"] = true,
+	["潮水圣殿"] = true,
 }
 
 local function changeGamma()
 	local MapId = C_Map.GetBestMapForUnit("player")
 	local hour, _ = GetGameTime()
-	if MapId and ID[MapId] then
+	local name = GetSubZoneText()
+	
+	if not MapId then return end
+	
+	if zoneID[MapId] then
 		-- set gamma here
 		SetCVar("Gamma", 1.2)
-	elseif MapId and timeID[MapId] then
-		if hour and (hour < 6 or hour > 18) then
+	elseif timeID[MapId] then
+		if hour and (hour < 6 or hour > 17) then
 			SetCVar("Gamma", 1.2)
+		else
+			SetCVar("Gamma", 1)
 		end
+	--elseif name == "潮水聖所" or name == "潮水圣殿" then
+		--SetCVar("Gamma", 1.2)
+	elseif nameList[name] then
+		SetCVar("Gamma", 1.2)
 	else
 		SetCVar("Gamma", 1)
 	end
 end
-
 
 local function eventHandler(self, event, ...)
 	changeGamma()
 end
 
 local CG = CreateFrame("Frame", "changeGamma")
-	CG:RegisterEvent("PLAYER_LOGIN")
+	--CG:RegisterEvent("PLAYER_LOGIN")
+	--CG:RegisterEvent("CHALLENGE_MODE_START")
+	--CG:RegisterEvent("PLAYER_REGEN_ENABLED")
 	CG:RegisterEvent("PLAYER_ENTERING_WORLD")
 	CG:RegisterEvent("ZONE_CHANGED")
 	CG:RegisterEvent("ZONE_CHANGED_INDOORS")
-	CG:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	CG:RegisterEvent("ZONE_CHANGED_NEW_AREA")	
 	CG:RegisterEvent("VARIABLES_LOADED")
-	--CG:RegisterEvent("PLAYER_REGEN_ENABLED")
 
 	CG:SetScript("OnEvent", eventHandler)
